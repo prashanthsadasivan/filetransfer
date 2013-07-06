22961a0c3b4afc03bc6c9793fe84da1fd7950259
Prashanth Sadasivan <sadpr@umich.edu>
2013-07-06 05:37:34 +0000

    big changes, styling, multitransfers

--- app/transfer/transfer.go
+++ app/transfer/transfer.go
@@ -4,14 +4,51 @@ import (
     revelpkg "github.com/robfig/revel"
     "io"
     "time"
+    "sync"
+    "fmt"
 )
 
-var (
-    sharedStream = make(chan byte)
-    filenamePipe = make(chan string, 1)
-    readerReady = make(chan bool, 1)
+
+type TransferConnection struct {
+    sharedStream chan byte
+    filenamePipe chan string
+    readerReady chan bool
     totalNumberOfChunks int64
-    numChunksSent = int64(0)
+    numChunksSent int64
+    key string
+}
+
+type BookKeeper struct {
+    connections map[string] *TransferConnection
+    mutex sync.Mutex
+}
+
+func (bk *BookKeeper) GetTransferForKey(key string) *TransferConnection {
+    bk.mutex.Lock()
+    defer bk.mutex.Unlock()
+    tc :=  bk.connections[key]
+    if tc == nil {
+        fmt.Printf("making tc for key: %s\n", key)
+        tc = new(TransferConnection)
+        tc.sharedStream = make(chan byte)
+        tc.filenamePipe = make(chan string, 1)
+        tc.readerReady = make(chan bool, 1)
+        tc.totalNumberOfChunks = int64(0)
+        tc.numChunksSent = int64(0)
+        bk.connections[key] = tc
+    }
+    return tc
+}
+
+func (bk *BookKeeper) DeleteTransferForKey(key string) {
+    bk.mutex.Lock()
+    defer bk.mutex.Unlock()
+    delete(bk.connections, key)
+    return
+}
+
+var (
+    TheBookKeeper BookKeeper
 )
 
 type StreamReader struct {
@@ -36,48 +73,56 @@ func (sr *StreamReader) Read(p []byte) (n int, err error) {
 
 // Return a file, either displayed inline or downloaded as an attachment.
 // The name and size are taken from the file info.
-func RenderBS(c *revelpkg.Controller, filename string) revelpkg.Result {
+func (tc *TransferConnection) RenderBS(c *revelpkg.Controller, filename string) revelpkg.Result {
     var (
         modtime       = time.Now()
     )
-    readerReady <- true
+    tc.readerReady <- true
     sr := new(StreamReader)
-    sr.New = sharedStream
+    sr.New = tc.sharedStream
     c.Response.Out.Header().Set("Content-Type", "application/octet-stream")
     return &revelpkg.BinaryResult{
         Reader:   sr,
         Name:     filename,
         Delivery: "attachment",
-        Length:   -1, // http.ServeContent gets the length itself
+        Length:   tc.totalNumberOfChunks*131072, // http.ServeContent gets the length itself
         ModTime:  modtime,
     }
 }
 
-func ReadyReceive(c *revelpkg.Controller) revelpkg.Result {
-    filename := <-filenamePipe
-    return RenderBS(c, filename)
+func (tc *TransferConnection) ReadyReceive(c *revelpkg.Controller) revelpkg.Result {
+    tc.key = <-tc.filenamePipe
+    return tc.RenderBS(c, tc.key)
 }
 
-func ReadySend(numChunks int64, filename string) bool{
-    totalNumberOfChunks = numChunks
-    filenamePipe <- filename
-    return <- readerReady
+func (tc *TransferConnection) ReadySend(numChunks int64, filename string) bool{
+    tc.totalNumberOfChunks = numChunks
+    tc.filenamePipe <- filename
+    return <- tc.readerReady
 }
 
-func SendChunk(chunk []byte) {
-    if(numChunksSent < totalNumberOfChunks) {
+func (tc *TransferConnection) SendChunk(chunk []byte) int64{
+    if(tc.numChunksSent < tc.totalNumberOfChunks) {
         for _,element := range chunk{
-            sharedStream <- element
+            tc.sharedStream <- element
         }
-        numChunksSent++;
-        if numChunksSent == totalNumberOfChunks {
-            close(sharedStream)
+        tc.numChunksSent++;
+        if tc.numChunksSent == tc.totalNumberOfChunks {
+            close(tc.sharedStream)
+            TheBookKeeper.DeleteTransferForKey(tc.key)
         }
+        return tc.numChunksSent
     } else {
         panic("wtf")
     }
 }
 
-func init() {
+func (tc *TransferConnection) Finished() bool {
+    return tc.numChunksSent == tc.totalNumberOfChunks
 }
+
+func init() {
+    TheBookKeeper.connections = make(map[string]*TransferConnection)
+}
+
 
