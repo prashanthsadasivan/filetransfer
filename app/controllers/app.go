a6144e2a391e66da442591c6fadda6d86fae9e27
Prashanth Sadasivan <prashanthsadasivan@gmail.com>
2014-05-17 05:49:53 -0700

    refactoring code

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,7 +2,7 @@ package controllers
 
 import (
 	"code.google.com/p/go.net/websocket"
-	"fmt"
+	"log"
 	revelpkg "github.com/revel/revel"
 	"hailmary/app/transfer"
 	"strconv"
@@ -35,77 +35,77 @@ func startTransfer(filename string, numChunks, fsize int64, ws *websocket.Conn) 
 }
 
 func (c App) SendChunk(ws *websocket.Conn) revelpkg.Result {
-	fmt.Printf("connected\n")
-	websocket.Message.Send(ws, "hi")
-	var data []byte
-	var filename string
-	numChunks := int64(-1)
-	fsize := int64(-1)
-
-	var tc *transfer.TransferConnection
-	for {
+    log.Printf("connected\n")
+    filename, numChunks, fsize, err := getFileMetadata(ws)
+    if err != nil {
+        panic(err)
+    }
+    tc := startTransfer(filename, numChunks, fsize, ws)
+    data := make([]byte, 1048576)
+    for {
 		startLoopTime := time.Now()
 		err := websocket.Message.Receive(ws, &data)
-		fmt.Printf("received msg: %d\n", len(data))
-		fmt.Printf("time since ws recieve start: %f\n ", time.Since(startLoopTime).Seconds())
-		if err != nil {
-			fmt.Printf(err.Error())
-			return nil
-		}
-		if len(data) < 100 {
-			msg := string(data)
-			arr := strings.FieldsFunc(msg, func(c rune) bool {
-				return c == '|'
-			})
-			if arr != nil && len(arr) == 2 {
-				if arr[0] == "filename" {
-					filename = arr[1]
-					if numChunks > 0 && fsize > 0 {
-						tc = startTransfer(filename, numChunks, fsize, ws)
-					}
+		log.Printf("received msg: %d\n", len(data))
+		log.Printf("time since ws recieve start: %f\n ", time.Since(startLoopTime).Seconds())
+        if err != nil {
+            panic(err)
+        }
+        if tc == nil {
+            panic("also not the correct protocol! no transferConnection available")
+        }
+        next_chunk := strconv.FormatInt(tc.SendChunk(data), 10)
+        if !tc.Finished() {
+            websocket.Message.Send(ws, "next|"+next_chunk)
+        } else {
+            websocket.Message.Send(ws, "end|end")
+        }
+    }
+}
 
-				} else if arr[0] == "numChunks" {
-					numChunks, err = strconv.ParseInt(arr[1], 10, 32)
-					fmt.Printf("number of chunks: %d\n", numChunks)
-					if filename != "" && fsize > 0 {
-						tc = startTransfer(filename, numChunks, fsize, ws)
-					}
-				} else if arr[0] == "size" {
-					fsize, err = strconv.ParseInt(arr[1], 10, 32)
-					if filename != "" && numChunks > 0 {
-						tc = startTransfer(filename, numChunks, fsize, ws)
-					}
-				} else {
-					panic("not the correct protocol!")
-				}
-			} else {
-				//if it doesn't match the protocol, just assume its data
-				if tc == nil {
-					panic("also not the correct protocol! no transferConnection available")
-				}
-				next_chunk := strconv.FormatInt(tc.SendChunk(data), 10)
-				if !tc.Finished() {
-					websocket.Message.Send(ws, "next|"+next_chunk)
-				} else {
-					websocket.Message.Send(ws, "end|end")
-				}
-			}
-
-		} else {
-			if tc == nil {
-				panic("holyballs")
-			}
-			sendChunkStart := time.Now()
-			next_chunk := strconv.FormatInt(tc.SendChunk(data), 10)
-			fmt.Printf("time to sendchunk: %f\n", time.Since(sendChunkStart).Seconds())
-			if !tc.Finished() {
-				websocket.Message.Send(ws, "next|"+next_chunk)
-			} else {
-				websocket.Message.Send(ws, "end|end")
-			}
-		}
-	}
-	return nil
+func getFileMetadata(ws *websocket.Conn) (filename string, numChunks int64, fsize int64, theerr error) {
+    data := make([]byte, 100)
+    for i := 0; i < 3; i++ {
+        err := websocket.Message.Receive(ws, &data)
+        if err != nil {
+            theerr = err
+            return
+        }
+        msg := string(data)
+        arr := strings.FieldsFunc(msg, func(c rune) bool {
+            return c == '|'
+        })
+        if arr == nil || len(arr) != 2 {
+            panic("protocol was breached!!")
+        }
+        if arr[0] == "filename" {
+            filename = arr[1]
+            if numChunks > 0 && fsize > 0 {
+                return
+            }
+        } else if arr[0] == "numChunks" {
+            numChunks, err = strconv.ParseInt(arr[1], 10, 32)
+            log.Printf("number of chunks: %d\n", numChunks)
+            if err != nil {
+                theerr = err
+                return
+            }
+            if filename != "" && fsize > 0 {
+                return
+            }
+        } else if arr[0] == "size" {
+            fsize, err = strconv.ParseInt(arr[1], 10, 32)
+            if err != nil {
+                theerr = err
+                return
+            }
+            if filename != "" && numChunks > 0 {
+                return
+            }
+        } else {
+            panic("not the correct protocol!")
+        }
+    }
+    panic("should not have reached this block")
 }
 
 func (c App) Index() revelpkg.Result {
